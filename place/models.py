30ea23d87d987ee951fc3bbae0798e4e631d3266
Mark <romanko@steelkiwi.com>
2015-11-18 10:22:44 +0200

    create Country model

--- place/models.py
+++ place/models.py
@@ -1,3 +1,9 @@
+# coding=utf-8
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+
+    def __unicode__(self):
+        return u'{}'.format(self.name)
