16d81bf70329a7b036594fd3f88a0b112021d5f1
Mark <romanko@steelkiwi.com>
2015-11-18 12:13:33 +0200

    change sellers, storage to ManyToManyField

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from person.models import Person
 from place.models import City
+from storage.models import Storage
 
 
 class TypeOfStore(models.Model):
@@ -14,8 +15,8 @@ class Store(models.Model):
     type = models.ForeignKey(TypeOfStore)
     name = models.CharField(max_length=80)
     owner = models.ForeignKey(Person, related_name='own_store')
-    sellers = models.ForeignKey(Person, related_name='seller_in')
-    storage = models.ForeignKey(Storage)
+    sellers = models.ManyToManyField(Person, related_name='seller_in')
+    storage = models.ManyToManyField(Storage)
     city = models.ForeignKey(City)
     address = models.CharField(max_length=250)
     site = models.URLField(blank=True, null=True)
