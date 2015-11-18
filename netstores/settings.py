612fdfd9e52feb70b29dc06cfd94f6337d37a204
Mark <romanko@steelkiwi.com>
2015-11-18 11:44:52 +0200

    connect all apps to INSTALLED_APPS

--- netstores/settings.py
+++ netstores/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'netstores',
+    'person',
+    'store',
+    'storage',
+    'place',
 )
 
 MIDDLEWARE_CLASSES = (
